8f034d548fbbed48e77490512dc3dd17647db3c7
damirTV <31268720+damirTV@users.noreply.github.com>
2019-01-31 22:57:04 +0300

    5 сделано

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(z3(10, 2, 3, 4));
         System.out.println(z4(1,22));
+        z5(-10);
     }
 
     private static int z3(int a, int b, int c, int d) {
@@ -15,6 +16,12 @@ public class Main {
         if ((a + b) >= 10 && (a + b) <= 20) {
             return true;
         } else return false;
+    }
+
+    private static void z5(int a) {
+        if (a >= 0) {
+            System.out.println("Передано положительное число");
+        } else System.out.println("Передано отрицательное число");
     }
 
 }
