9a8e432b9157869443a520fa1ac267c0715d64ce
damirTV <31268720+damirTV@users.noreply.github.com>
2019-01-31 22:53:50 +0300

    задание 4

--- Main.java
+++ Main.java
@@ -3,12 +3,18 @@ package ru.geekbrains.lesson_1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(z3(10,2,3,4));
+        System.out.println(z3(10, 2, 3, 4));
+        System.out.println(z4(1,22));
     }
 
     private static int z3(int a, int b, int c, int d) {
-         return a * (b + (c / d));
+        return a * (b + (c / d));
     }
 
+    private static boolean z4(int a, int b) {
+        if ((a + b) >= 10 && (a + b) <= 20) {
+            return true;
+        } else return false;
+    }
 
 }
