bd5a6562d957eba416baf5d2e4c27d29a0ae2561
damirTV <31268720+damirTV@users.noreply.github.com>
2019-01-31 22:30:40 +0300

    1

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package ru.geekbrains.lesson_1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello"); // Hello World efdfe
+        System.out.println("Hello"); // Hello Wor
     }
 }
