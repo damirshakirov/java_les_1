f15d290feaa6f45f9196671f729c50b38cd26af1
damirTV <31268720+damirTV@users.noreply.github.com>
2019-01-31 22:47:09 +0300

    Задание 3 сделано

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ package ru.geekbrains.lesson_1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello"); // Hello Wor
+        System.out.println(z3(10,2,3,4));
     }
+
+    private static int z3(int a, int b, int c, int d) {
+         return a * (b + (c / d));
+    }
+
+
 }
